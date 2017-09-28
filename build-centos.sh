aed0dc0c1e29fcfa1ce66eaa270116830ae22bfc
Ali Ahmed <a.ahmed@streaml.io>
2017-09-27 17:51:37 -0700

    Update

---
+++ build-centos.sh
@@ -0,0 +1,14 @@
+export USER=build-server;
+cd /src;
+python bazel_configure.py;
+bazel build -c opt --config=centos scripts/packages:binpkgs;
+chown 1000:1000 bazel-bin/scripts/packages/heron-api-install.sh;
+chown 1000:1000 bazel-bin/scripts/packages/heron-client-install.sh;
+chown 1000:1000 bazel-bin/scripts/packages/heron-tools-install.sh;
+chown 1000:1000 bazel-bin/scripts/packages/heron-core.tar.gz;
+mkdir /src/artifacts;
+chown -R 1000:1000 /src/artifacts;
+cp bazel-bin/scripts/packages/heron-api-install.sh /src/artifacts/;
+cp bazel-bin/scripts/packages/heron-client-install.sh /src/artifacts/;
+cp bazel-bin/scripts/packages/heron-tools-install.sh /src/artifacts/;
+cp bazel-bin/scripts/packages/heron-core.tar.gz /src/artifacts/;
